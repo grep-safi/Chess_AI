a61ca5ea7098828ea2b59b666bce4d122450d476
Safi Mohammed <safimohammed@Safis-MBP.hsd1.ca.comcast.net>
2020-02-16 15:44:04 -0800

    Initial commit + setting up a vague outline of where to start

---
+++ Chess.py
@@ -0,0 +1,33 @@
+class Chess:
+    def __init__(self):
+        pass
+
+
+class Pawn(Chess):
+    def __init__(self):
+        pass
+
+
+class Rook(Chess):
+    def __init__(self):
+        pass
+
+
+class Knight(Chess):
+    def __init__(self):
+        pass
+
+
+class Bishop(Chess):
+    def __init__(self):
+        pass
+
+
+class Queen(Chess):
+    def __init__(self):
+        pass
+
+
+class King(Chess):
+    def __init__(self):
+        pass
